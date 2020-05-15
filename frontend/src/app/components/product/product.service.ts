05af1284b15ae3e86921331222ed621d3c0f21de
Sidão <sidaoswat@hotmail.com>
2020-05-15 01:50:53 -0300

    create method put and get products

--- frontend/src/app/components/product/product.service.ts
+++ frontend/src/app/components/product/product.service.ts
@@ -29,4 +29,14 @@ export class ProductService {
   public read(): Observable<Product[]> {
     return this.http.get<Product[]>(this.baseUrl);
   }
+
+  public readById(id: string): Observable<Product> {
+    const url = `${this.baseUrl}/${id}`;
+    return this.http.get<Product>(url);
+  }
+
+  public update(product: Product): Observable<Product> {
+    const url = `${this.baseUrl}/${product.id}`
+    return this.http.put<Product>(url, product);
+  }
 }
